bb51a0611314fa3876093081d7f06dfedf386356
Taras Mankovski <taras@quandl.com>
2014-05-17 12:18:58 -0400

    Added ability to page root and page by relative or absolute path

--- tests/page.js
+++ tests/page.js
@@ -15,8 +15,8 @@ test("toPage with nested objects", function(t){
     'file1.html': "---\ntitle: HTML Title\n---\nHTML Body",
     'file2.md': "---\ntitle: Markdown Title\n---\nMarkdown **Body**",
     'about': {
-      'file1.html': "---\ntitle: Handlebars Title\n---\nHandlebars {{body}}",
-      'file2.md': "---\ntitle: Markdown Title\n---\nMarkdown **Body**"
+      'file1.html': "---\ntitle: About -> File1\n---\nHandlebars {{body}}",
+      'file2.md': "---\ntitle: About -> File2\n---\nMarkdown **Body**"
     }
   };
 
@@ -25,17 +25,43 @@ test("toPage with nested objects", function(t){
 
   var page = toPage(this.tmpDest);
 
-  t.type(page.pages, 'object');
-  t.type(page.title, 'string');
-  t.type(page.body, 'string');
-  t.equal(page.title, 'Index File');
-  t.equal(page.body, 'Index Body');
+  t.test("presence & types", function(t) {
+    t.plan(9);
+    t.type(page._, 'object');
+    t.type(page.title, 'string');
+    t.type(page.body, 'string');
+    t.equal(page.title, 'Index File');
+    t.equal(page.body, 'Index Body');
+    t.type(page._.about, 'object');
+    t.equal(page._.about.parent, page);
+    t.type(page._.about._.file1, 'object');
+    t.equal(page._.about._.file1.parent, page._.about);
+    t.end();
+  });
 
-  t.type(page.pages.about, 'object');
-  t.equal(page.pages.about.parent, page);
+  t.test('paths', function(t){
+    t.plan(3);
+    t.equal(page._path, '');
+    t.equal(page._.file1._path, 'file1');
+    t.equal(page._.about._.file1._path, 'about/file1');
+    t.end();
+  });
 
-  t.type(page.pages.about.pages.file1, 'object');
-  t.equal(page.pages.about.pages.file1.parent, page.pages.about);
+  t.test('getRoot', function(t){
+    t.plan(3);
+    t.equal(page.getRoot(), page);
+    t.equal(page._.about.getRoot(), page);
+    t.equal(page._.about._.file1.getRoot(), page);
+    t.end();
+  });
+
+  t.test('getPage', function(t){
+    t.equal(page.getPage('about/file1'), page._.about._.file1, "about/file1");
+    t.type(page.getPage(), 'null', 'no param, returns null');
+    t.equal(page._.about.getPage(''), page._.about, 'empty string returns called page');
+    t.equal(page._.about._.file1.getPage('/file2'), page._.file2, 'absolute returns from root');
+    t.end();
+  });
 
   t.end();
 });
