80eb8ddddfa9f3018c121a9b8c50efc7d881e97f
Taras Mankovski <taras@quandl.com>
2014-05-22 08:34:08 -0400

    Added markdown test

--- tests/filters/markdown.js
+++ tests/filters/markdown.js
@@ -1,36 +1,10 @@
 var test = require('tap').test;
-var jsdom = require('jsdom');
-var jquery = require('jquery');
 
-function $(html) {
+var MarkdownFilter = require('../../lib/filters/markdown');
 
-  /* parse the html and create a dom window */
-  var window = jsdom.jsdom(html, null, {
-    // standard options:  disable loading other assets
-    // or executing script tags
-    FetchExternalResources: false,
-    ProcessExternalResources: false,
-    MutationEvents: false,
-    QuerySelector: false
-  }).createWindow();
+var filter = new MarkdownFilter('mock');
 
-  return jquery.create(window);
-}
-
-
-var processPage = require('../../lib/filters/markdown').processPage;
-
-test('render POJO page', function(t){
-
-  var html = processPage({
-    title: 'Hello World',
-    body: "This is a **beautiful** world."
-  });
-
-  var doc = $(html);
-
-  t.equal(doc.find('p').text(), 'This is a beautiful world.');
-  t.equal(doc.find('strong').text(), 'beautiful');
-
+test('markdown renders', function(t){
+  t.equal(filter.processPage('Hello **World**', null), '<p>Hello <strong>World</strong></p>');
   t.end();
 });
